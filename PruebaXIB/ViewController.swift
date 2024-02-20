a9730280b9ce609bb90349bf744e642479f9e2be
Jesus <jesusguerrerogonzalez2@gmail.com>
2024-02-20 00:17:09 -0800

    Initial Commit

---
+++ PruebaXIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaXIB
+//
+//  Created by Jesus on 20/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
